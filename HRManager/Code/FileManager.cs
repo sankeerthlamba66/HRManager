0ab31c1542f2b779ba77329d2531c1ec91a0e531
Sankeerth <sankeerthyadav66@gmail.com>
2022-01-25 10:52:21 +0530

    File Upload in Helper

---
+++ HRManager/Code/FileManager.cs
@@ -0,0 +1,23 @@
+﻿namespace HRManager.Code
+{
+    public class FileManager
+    {
+        public static string UploadProfile(IFormFile Document, string UploadFolderPath)
+        {
+            string uniqueFileName = string.Empty;
+            string filePath = string.Empty;
+
+            if (Document != null)
+            {
+                uniqueFileName = Guid.NewGuid().ToString() + "_" + Document.FileName;
+                filePath = Path.Combine(UploadFolderPath, uniqueFileName);
+                using (var fileStream = new FileStream(filePath, FileMode.Create))
+                {
+                    Document.CopyTo(fileStream);
+                }
+            }
+
+            return filePath;
+        }
+    }
+}

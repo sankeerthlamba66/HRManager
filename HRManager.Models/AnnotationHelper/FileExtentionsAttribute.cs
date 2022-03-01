2af668c24e6408a0cb0ff99315a4b8478dc7c298
Sankeerth <sankeerthyadav66@gmail.com>
2022-03-01 17:40:12 +0530

    FileAttribute is created

---
+++ HRManager.Models/AnnotationHelper/FileExtentionsAttribute.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRManager.Models.AnnotationHelper
+{
+    internal class FileExtentionsAttribute:ValidationAttribute
+    {
+        public string Extensions { get; set; } = "png,jpg,jpeg,pdf";
+        public override bool IsValid(object? value)
+        {
+            bool IsValid = true;
+            IFormFile? file = value as IFormFile;
+            List<string> allowedExtensions = this.Extensions.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries).ToList();
+            if (file is not null)
+            {
+                var fileName=file.FileName;
+                IsValid = allowedExtensions.Any(y=>fileName.EndsWith(y));
+            }
+            
+
+            return IsValid;
+        }
+    }
+}
--- HRManager.Models/EntityViews/EmployeeFileDocument.cs
+++ HRManager.Models/EntityViews/EmployeeFileDocument.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using HRManager.Models.AnnotationHelper;
+using Microsoft.AspNetCore.Http;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
@@ -14,75 +15,75 @@ namespace HRManager.Models.EntityViews
         public int UserId { get; set; }
 
         [Display(Name = "Passport Photo")]
-        [FileExtensions(Extensions = "jpg,jpeg,png", ErrorMessage = "File format should be of type jpg,jpeg,png")]
+        [FileExtentions(Extensions = "jpg,jpeg,png", ErrorMessage = "File format should be of type jpg,jpeg,png")]
         public IFormFile? PassportPhoto { get; set; }
 
         [Display(Name = "Resume")]
-        [FileExtensions(Extensions = "pdf", ErrorMessage = "File format should be of type pdf")]
+        [FileExtentions(Extensions = "pdf", ErrorMessage = "File format should be of type pdf")]
         public IFormFile? Resume { get; set; }
 
         [Display(Name = "Pan Card")]
-        [FileExtensions(Extensions = "jpg,jpeg,png", ErrorMessage = "File format should be of type jpg,jpeg,png")]
+        [FileExtentions(Extensions = "jpg,jpeg,png", ErrorMessage = "File format should be of type jpg,jpeg,png")]
         public IFormFile? PanCard { get; set; }
 
         [Display(Name = "Aadhar Card")]
-        [FileExtensions(Extensions = "jpg,jpeg,png", ErrorMessage = "File format should be of type jpg,jpeg,png")]
+        [FileExtentions(Extensions = "jpg,jpeg,png", ErrorMessage = "File format should be of type jpg,jpeg,png")]
         public IFormFile? AadharCard { get; set; }
 
         [Display(Name = "Passport")]
-        [FileExtensions(Extensions = "jpg,jpeg,png", ErrorMessage = "File format should be of type jpg,jpeg,png")]
+        [FileExtentions(Extensions = "jpg,jpeg,png", ErrorMessage = "File format should be of type jpg,jpeg,png")]
         public IFormFile? Passport { get; set; }
 
         [Display(Name = "Voter Id")]
-        [FileExtensions(Extensions = "jpg,jpeg,png", ErrorMessage = "File format should be of type jpg,jpeg,png")]
+        [FileExtentions(Extensions = "jpg,jpeg,png", ErrorMessage = "File format should be of type jpg,jpeg,png")]
         public IFormFile? VoterId { get; set; }
 
         [Display(Name = "Current Address Proof")]
-        [FileExtensions(Extensions = "jpg,jpeg,png", ErrorMessage = "File format should be of type jpg,jpeg,png")]
+        [FileExtentions(Extensions = "jpg,jpeg,png", ErrorMessage = "File format should be of type jpg,jpeg,png")]
         public IFormFile? CurrentAddressProof { get; set; }
 
         [Display(Name = "Permanent Address Proof")]
-        [FileExtensions(Extensions = "jpg,jpeg,png", ErrorMessage = "File format should be of type jpg,jpeg,png")]
+        [FileExtentions(Extensions = "jpg,jpeg,png", ErrorMessage = "File format should be of type jpg,jpeg,png")]
         public IFormFile? PermanentAddressProof { get; set; }
 
         [Display(Name = "Father's Aadhar Card")]
-        [FileExtensions(Extensions = "jpg,jpeg,png", ErrorMessage = "File format should be of type jpg,jpeg,png")]
+        [FileExtentions(Extensions = "jpg,jpeg,png", ErrorMessage = "File format should be of type jpg,jpeg,png")]
         public IFormFile? FathersAadharCard { get; set; }
 
         [Display(Name = "Mother's Aadhar Card")]
-        [FileExtensions(Extensions = "jpg,jpeg,png", ErrorMessage = "File format should be of type jpg,jpeg,png")]
+        [FileExtentions(Extensions = "jpg,jpeg,png", ErrorMessage = "File format should be of type jpg,jpeg,png")]
         public IFormFile? MothersAadharCard { get; set; }
 
         [Display(Name = "Three Months Bank Statement Of Salary Account")]
-        [FileExtensions(Extensions = "jpg,jpeg,png,pdf", ErrorMessage = "File format should be of type jpg,jpeg,png,pdf")]
+        [FileExtentions(Extensions = "jpg,jpeg,png,pdf", ErrorMessage = "File format should be of type jpg,jpeg,png,pdf")]
         public IFormFile? ThreeMonthsBankStatementOfSalaryAccount { get; set; }
 
         [Display(Name = "Form 16 Or Income Certificate Of Current Fin Year")]
-        [FileExtensions(Extensions = "jpg,jpeg,png,pdf", ErrorMessage = "File format should be of type jpg,jpeg,png,pdf")]
+        [FileExtentions(Extensions = "jpg,jpeg,png,pdf", ErrorMessage = "File format should be of type jpg,jpeg,png,pdf")]
         public IFormFile? Form16OrIncomeCertificateOfCurrentFinYear { get; set; }
 
         [Display(Name = "SSC Or Equivalent")]
-        [FileExtensions(Extensions = "jpg,jpeg,png,pdf", ErrorMessage = "File format should be of type jpg,jpeg,png,pdf")]
+        [FileExtentions(Extensions = "jpg,jpeg,png,pdf", ErrorMessage = "File format should be of type jpg,jpeg,png,pdf")]
         public IFormFile? SSCOrEquivalent { get; set; }
 
         [Display(Name = "Intermediate Or Equivalent")]
-        [FileExtensions(Extensions = "jpg,jpeg,png,pdf", ErrorMessage = "File format should be of type jpg,jpeg,png,pdf")]
+        [FileExtentions(Extensions = "jpg,jpeg,png,pdf", ErrorMessage = "File format should be of type jpg,jpeg,png,pdf")]
         public IFormFile? IntermediateOrEquivalent { get; set; }
 
         [Display(Name = "Graduation Or Equivalent")]
-        [FileExtensions(Extensions = "jpg,jpeg,png,pdf", ErrorMessage = "File format should be of type jpg,jpeg,png,pdf")]
+        [FileExtentions(Extensions = "jpg,jpeg,png,pdf", ErrorMessage = "File format should be of type jpg,jpeg,png,pdf")]
         public IFormFile? GraduationOrEquivalent { get; set; }
 
         [Display(Name = "PG Or Equivalent ")]
-        [FileExtensions(Extensions = "jpg,jpeg,png,pdf", ErrorMessage = "File format should be of type jpg,jpeg,png,pdf")]
+        [FileExtentions(Extensions = "jpg,jpeg,png,pdf", ErrorMessage = "File format should be of type jpg,jpeg,png,pdf")]
         public IFormFile? PGOrEquivalent { get; set; }
 
         [Display(Name = "Advanced Diploma If Any")]
-        [FileExtensions(Extensions = "jpg,jpeg,png,pdf", ErrorMessage = "File format should be of type jpg,jpeg,png,pdf")]
+        [FileExtentions(Extensions = "jpg,jpeg,png,pdf", ErrorMessage = "File format should be of type jpg,jpeg,png,pdf")]
         public IFormFile? AdvancedDiplomaIfAny { get; set; }
 
         [Display(Name = "Professional Certifications If Any")]
-        [FileExtensions(Extensions = "jpg,jpeg,png,pdf", ErrorMessage = "File format should be of type jpg,jpeg,png,pdf")]
+        [FileExtentions(Extensions = "jpg,jpeg,png,pdf", ErrorMessage = "File format should be of type jpg,jpeg,png,pdf")]
         public IFormFile? ProfessionalCertificationsIfAny { get; set; }
     }
 }
